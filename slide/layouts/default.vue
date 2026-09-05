05ae0f2a50ade25354c9f45cf47f9658e9911ad7
ayano-yuki <kd0ayano@gmail.com>
2026-09-06 04:42:58 +0900

    fix: defaultレイアウトに箇条書きのスタイルを追加

--- slide/layouts/default.vue
+++ slide/layouts/default.vue
@@ -4,7 +4,9 @@ import { COLOR_BLACK, COLOR_PRIMARY, COLOR_WHITE } from './constants';
 
 <template>
   <div class="default">
-    <slot />
+    <div class="default-slot">
+      <slot />
+    </div>
   </div>
 </template>
 
@@ -17,6 +19,11 @@ import { COLOR_BLACK, COLOR_PRIMARY, COLOR_WHITE } from './constants';
   box-sizing: border-box;
   padding: 1rem 2rem !important;
   background: v-bind("COLOR_WHITE") !important;
+}
+
+.default-slot {
+  width: 100%;
+  height: 100%;
 }
 
 /* Heading */
@@ -40,10 +47,76 @@ import { COLOR_BLACK, COLOR_PRIMARY, COLOR_WHITE } from './constants';
 }
 
 /* Body */
-.default p {
-  margin: 0;
+.default-slot p {
+  margin: 0 0 0.5rem;
   color: v-bind("COLOR_BLACK");
   font-size: 1.25rem;
   font-weight: 600;
+  line-height: 1.5;
+}
+
+.default-slot ul,
+.default-slot ol {
+  display: block !important;
+  margin: 0 0 0.5rem;
+  color: v-bind("COLOR_BLACK");
+  font-size: 1.25rem !important;
+  font-weight: 600;
+  line-height: 1.5;
+  list-style-position: outside !important;
+}
+
+.default-slot ul {
+  padding-left: 1.5rem !important;
+  list-style-type: disc !important;
+}
+
+.default-slot ol {
+  padding-left: 1.75rem !important;
+  list-style-type: decimal !important;
+}
+
+.default-slot ul ul {
+  margin-top: 0.25rem;
+  padding-left: 1.5rem !important;
+  list-style-type: circle !important;
+}
+
+.default-slot ul ul ul {
+  list-style-type: square !important;
+}
+
+.default-slot ul ul ul ul {
+  list-style-type: disc !important;
+}
+
+.default-slot ol ol {
+  margin-top: 0.25rem;
+  padding-left: 1.75rem !important;
+  list-style-type: lower-alpha !important;
+}
+
+.default-slot ol ol ol {
+  list-style-type: lower-roman !important;
+}
+
+.default-slot li {
+  display: list-item !important;
+  margin: 0 0 0.5rem;
+  padding-left: 0.25rem;
+}
+
+.default-slot li > ul,
+.default-slot li > ol {
+  margin-top: 0.25rem;
+  margin-bottom: 0.25rem;
+}
+
+.default-slot > :first-child {
+  margin-top: 0;
+}
+
+.default-slot > :last-child {
+  margin-bottom: 0;
 }
 </style>
